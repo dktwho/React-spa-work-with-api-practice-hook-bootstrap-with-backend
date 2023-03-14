c67c804b629eb4b8c3cf05f7012ea3836ac41ec2
Alexander Korobov <dktwho@mail.ru>
2023-03-14 12:19:27 +0300

    modified component random cat, add useEffect and render, add navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+      </div>
+    </nav>
   )
 }
 
--- client/src/components/RandomCats/RandomCats.js
+++ client/src/components/RandomCats/RandomCats.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const RandomCats = () => {
+  const [img, setImg] = useState(null)
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response=>response.json())
+    .then(data => setImg(data[0].url) )
+
+  }, )
   return (
-    <div>RandomCats</div>
+    <>
+      {img &&  (
+      <div className="card" style={{width: '18rem'}}>
+        <img src={img} className="card-img-top" alt="randomcat"/>
+        <div className="card-body">
+        </div>
+      </div>
+    ) }
+    </>
+
+   
   )
 }
 
