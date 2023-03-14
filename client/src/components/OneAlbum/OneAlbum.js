2f0b14a8c7274eaabe23492a701ed1ac9ad73337
Alexander Korobov <dktwho@mail.ru>
2023-03-14 14:30:21 +0300

    styled loader, and conditional render in component onealbum with useParams

--- client/src/components/OneAlbum/OneAlbum.js
+++ client/src/components/OneAlbum/OneAlbum.js
@@ -5,28 +5,33 @@ import Loader from '../Loader/Loader'
 const OneAlbum = () => {
 
   const {id} = useParams()
-  const [info, setInfo] = useState({})
+  const [info, setInfo] = useState(null)
   const [loader, setLoader] = useState(false)
+
   useEffect(() => {
     setLoader(true)
-    fetch(`https://itunes.apple.com/lookup?id=${id}`)
-    .then(response => response.json())
-    .then(({results}) => setInfo(results[0]) )
-    setLoader(false)
+    setTimeout(() => {
+      fetch(`https://itunes.apple.com/lookup?id=${id}`)
+      .then(response => response.json())
+      .then(({results}) => setInfo(results[0]) )
+      setLoader(false)
+    }, 1000)
+    
   }, [])
   
 
   return (
     <>
     {loader && <Loader/>}
-    <ul className="list-group">
+    {info &&  <ul className="list-group">
       <li className="list-group-item">Name: {info.collectionName}</li>
       <li className="list-group-item">Number of tracks: {info.trackCount}</li>
       <li className="list-group-item">Price: {info.collectionPrice}</li>
       <li className="list-group-item">Currency: {info.currency}</li>
       <li className="list-group-item">Release date: {new Date(info.releaseDate).toLocaleDateString()}</li>
       <li className="list-group-item">Genre: {info.primaryGenreName}</li>
-    </ul>
+    </ul>  }
+   
 </>
   )
 }
