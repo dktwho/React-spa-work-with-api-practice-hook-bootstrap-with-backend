b8bddc4870d1fdee0f8f32ff3f66e8a83500e846
Alexander Korobov <dktwho@mail.ru>
2023-03-14 11:56:14 +0300

    init project with 2 directory: client and server with dependencies

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+
+
+app.get('/meme', async (req, res) => {
+  const response = await fetch(
+    `http://apimeme.com/meme?meme=2nd-Term-Obama&top=${req.query.top}&bottom=${req.query.bottom}`
+  );
+  res.json(response.url);
+});
+
+app.listen(3002, () => console.log('server start'));
